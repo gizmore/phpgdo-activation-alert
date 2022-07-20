215c63c2fa324c651e361bbcccf682c90e62ed9e
gizmore <gizmore@wechall.net>
2022-07-20 07:54:55 +0200

    '7.0.0-r1374'

--- lang/activation_alert_en.php
+++ lang/activation_alert_en.php
@@ -5,13 +5,13 @@ return [
         
     'mail_subj_user_activated_staff' => '[%s] New user activated',
     'mail_body_user_activated_staff' => '
-    Hello %s,
+Hello %s,
     
-    There has been a user activated on %s.
+There has been a user activated on %s.
     
-    Username: %s
-    IP: %s
+Username: %s
+IP: %s
     
-    Kind Regards,
-    The %2$s robot',
+Kind Regards,
+The %2$s robot',
 ];
