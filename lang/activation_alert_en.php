39c64cf1b28a4c5125a4766728d9c50de5ad23af
gizmore <gizmore@wechall.net>
2022-09-08 01:04:45 +0200

    '7.0.1-r1581 debugging and fixes'

--- lang/activation_alert_en.php
+++ lang/activation_alert_en.php
@@ -2,8 +2,8 @@
 namespace GDO\ActivationAlert\lang;
 return [
     'cfg_activation_alert_mail_receiver' => 'Mail receiver for activation alerts',
-        
-    'mail_subj_user_activated_staff' => '[%s] New user activated',
+
+	'mail_subj_user_activated_staff' => '[%s] New user activated',
     'mail_body_user_activated_staff' => '
 Hello %s,
     
