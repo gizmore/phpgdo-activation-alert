ccb47274c58fa5fd5937accbf059563f0f4d169f
gizmore <gizmore@wechall.net>
2022-10-07 07:01:20 +0200

    '7.0.1-r1649 German Lang Pack - I'

---
+++ lang/activation_alert_de.php
@@ -0,0 +1,13 @@
+<?php
+namespace GDO\ActivationAlert\lang;
+return [
+    'cfg_activation_alert_mail_receiver' => 'E-Mail-Empfänger für Aktivierungsbenachrichtigungen','mail_subj_user_activated_staff' => '[%s] Neuer Benutzer aktiviert','mail_body_user_activated_staff' => 'Hallo %s,
+    
+Auf %s wurde ein Benutzer aktiviert.
+    
+Benutzername: %s
+IP: %s
+    
+Mit freundlichen Grüßen,
+Das %2$s Team',
+];
